50d1f2c2f5b2e2a39d166312f4e861d58767684b
Chromium1 <priyesh.96@hotmail.com>
2014-06-21 10:44:08 -0400

    Add double tap listener

--- src/com/priyesh/hexatime/HexatimeService.java
+++ src/com/priyesh/hexatime/HexatimeService.java
@@ -8,14 +8,17 @@ import android.graphics.Paint;
 import android.graphics.Typeface;
 import android.os.Handler;
 import android.service.wallpaper.WallpaperService;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
+import android.widget.Toast;
 
 public class HexatimeService extends WallpaperService {
 
 	private static final String TAG = "Wallpaper";
 
 	public int oneSecond = 1000;
-	
+
 	public int hour, min, sec;
 	public Calendar cal;
 
@@ -23,14 +26,20 @@ public class HexatimeService extends WallpaperService {
 	public Engine onCreateEngine() {
 		return new DemoWallpaperEngine();
 	}
+
 	private class DemoWallpaperEngine extends Engine {
+
 		private boolean mVisible = false;
 		private final Handler mHandler = new Handler();
+		private GestureDetector mGestureDetector;
+
 		private final Runnable mUpdateDisplay = new Runnable() {
+
 			@Override
 			public void run() {
 				draw();
 			}};
+
 			private void draw() {
 				cal = Calendar.getInstance();
 				hour = cal.get(Calendar.HOUR_OF_DAY);
@@ -45,9 +54,9 @@ public class HexatimeService extends WallpaperService {
 						p.setTextSize(90);
 						p.setTypeface(Typeface.createFromAsset(getAssets(), "LatoLight.ttf"));
 						p.setAntiAlias(true);
-						
+
 						String text = String.format("#%02d%02d%02d", hour, min, sec );
-						
+
 						float d = p.measureText(text, 0, text.length());
 						int offset = (int) d / 2;
 						int w = c.getWidth();
@@ -67,6 +76,7 @@ public class HexatimeService extends WallpaperService {
 					mHandler.postDelayed(mUpdateDisplay, oneSecond);
 				}
 			}
+
 			@Override
 			public void onVisibilityChanged(boolean visible) {
 				mVisible = visible;
@@ -76,21 +86,48 @@ public class HexatimeService extends WallpaperService {
 					mHandler.removeCallbacks(mUpdateDisplay);
 				}
 			}
+
+			@Override
+			public void onCreate(SurfaceHolder holder) {
+				super.onCreate(holder);
+				mGestureDetector = new GestureDetector(HexatimeService.this, mGestureListener);
+				setTouchEventsEnabled(true);
+			}
+
 			@Override
 			public void onSurfaceChanged(SurfaceHolder holder, int format, int width, int height) {
 				draw();
 			}
+
 			@Override
 			public void onSurfaceDestroyed(SurfaceHolder holder) {
 				super.onSurfaceDestroyed(holder);
 				mVisible = false;
 				mHandler.removeCallbacks(mUpdateDisplay);
 			}
+
 			@Override
 			public void onDestroy() {
 				super.onDestroy();
 				mVisible = false;
 				mHandler.removeCallbacks(mUpdateDisplay);
 			}
+
+			@Override 
+			public void onTouchEvent(MotionEvent event) {
+				super.onTouchEvent(event);
+				mGestureDetector.onTouchEvent(event);
+
+			}
+
+			private GestureDetector.OnGestureListener mGestureListener = new GestureDetector.SimpleOnGestureListener() {
+				@Override
+				public boolean onDoubleTap(MotionEvent e) {
+					Toast.makeText(getApplicationContext(), "You have double tapped",
+							Toast.LENGTH_SHORT).show();
+					
+					return true;
+				}
+			};
 	}
 }
