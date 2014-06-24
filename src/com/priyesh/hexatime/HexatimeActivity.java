39d794e67c4d4f93c7e5020ae1840dae4238d5bb
Chromium1 <priyesh.96@hotmail.com>
2014-06-24 15:12:25 -0400

    Animate background color changing

--- src/com/priyesh/hexatime/HexatimeActivity.java
+++ src/com/priyesh/hexatime/HexatimeActivity.java
@@ -1,12 +1,20 @@
 package com.priyesh.hexatime;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ValueAnimator;
+import android.animation.ValueAnimator.AnimatorUpdateListener;
 import android.app.Activity;
 import android.app.WallpaperManager;
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.TransitionDrawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -14,29 +22,64 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
-public class HexatimeActivity extends Activity {
+public class HexatimeActivity extends Activity implements Handler.Callback{
 
+	private Handler mHandler = new Handler(this);
+
+    private int mDelay = 1500; // Delay in milliseconds.
+    
 	LinearLayout layout;
-    private IntroView mIntroView;
-	 
+	private IntroView mIntroView;
+	TransitionDrawable trans;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState); 
 		setContentView(R.layout.hexatime_activity);
- 
+
 		layout =(LinearLayout)findViewById(R.id.linearlayout);
-		
+
 		mIntroView = (IntroView) findViewById(R.id.intro);
-	    mIntroView.setSvgResource(R.raw.hexatime);
-	    
-	    layout.setBackgroundDrawable(new ColorDrawable (getResources().getColor(R.color.c1)));
-	    
-        
+		mIntroView.setSvgResource(R.raw.hexatime);
+
+	//	layout.setBackgroundDrawable(new ColorDrawable (getResources().getColor(R.color.c1)));
+
+		ColorDrawable c1 = new ColorDrawable(getResources().getColor(R.color.c1));
+		ColorDrawable c2 = new ColorDrawable(getResources().getColor(R.color.c2));
+		ColorDrawable[] color = {c1, c2}; 
+		trans = new TransitionDrawable(color);
+		layout.setBackgroundDrawable(trans); 
+		start();
+
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
 			Window win = getWindow();
 			win.setFlags (WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION, WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
 			win.setFlags (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
 		}
 	}
+	
+	private Runnable mEvent = new Runnable() {
+        public void run() {
+            mHandler.removeCallbacks(mEvent);
+            mHandler.postDelayed(mEvent, mDelay);
+            Message message = mHandler.obtainMessage();
+            mHandler.sendMessage(message);
+        }
+    };
+
+    public boolean handleMessage(Message message) {
+    	TransitionDrawable drawable = (TransitionDrawable) layout.getBackground();    
+    	drawable.reverseTransition(mDelay);
+        return true;
+    }
+
+    public void start() {
+        mHandler.post(mEvent);
+    }
+
+    public void stop() {
+        mHandler.removeCallbacks(mEvent);
+    }
+
 
 }
