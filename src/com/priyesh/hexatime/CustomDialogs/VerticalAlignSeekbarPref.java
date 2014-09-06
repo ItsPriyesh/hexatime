7ccaa3a8ebcab63d4e0845c647d3822d030e7c70
Priyesh Patel <priyesh.96@hotmail.com>
2014-09-05 23:31:56 -0400

    Delete commented out code

--- src/com/priyesh/hexatime/CustomDialogs/VerticalAlignSeekbarPref.java
+++ src/com/priyesh/hexatime/CustomDialogs/VerticalAlignSeekbarPref.java
@@ -71,72 +71,7 @@ public class VerticalAlignSeekbarPref extends DialogPreference {
 	public VerticalAlignSeekbarPref(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		setDialogLayoutResource(R.layout.vertical_align_seekbar);
-	//	setup(context, attrs);
 	}
-
-	/*	public VerticalAlignSeekbarPref(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
-		setup(context, attrs);
-	}
-
-	private void setup(Context context, AttributeSet attrs) {
-		setDialogLayoutResource(R.layout.vertical_align_seekbar);
-	}
-
-	@Override
-	protected Object onGetDefaultValue(TypedArray a, int index) {
-		return a.getFloat(index, 0);
-	}
-
-	@Override
-	protected void onSetInitialValue(boolean restoreValue, Object defaultValue) {
-		setValue(restoreValue ? getPersistedFloat(mValue) : (Float) defaultValue);
-	}
-
-	@Override
-	public CharSequence getSummary() {
-		if (mSummaries != null && mSummaries.length > 0) {
-			int index = (int) (mValue * mSummaries.length);
-			index = Math.min(index, mSummaries.length - 1);
-			return mSummaries[index];
-		} else {
-			return super.getSummary();
-		}
-	}
-
-	public void setSummary(CharSequence[] summaries) {
-		mSummaries = summaries;
-	}
-
-	@Override
-	public void setSummary(CharSequence summary) {
-		super.setSummary(summary);
-		mSummaries = null;
-	}
-
-	@Override
-	public void setSummary(int summaryResId) {
-		try {
-			setSummary(getContext().getResources().getStringArray(summaryResId));
-		} catch (Exception e) {
-			super.setSummary(summaryResId);
-		}
-	}
-
-	public float getValue() {
-		return mValue;
-	}
-
-	public void setValue(float value) {
-		value = Math.max(0, Math.min(value, 1)); // clamp to [0, 1]
-		if (shouldPersist()) {
-			persistFloat(value);
-		}
-		if (value != mValue) {
-			mValue = value;
-			notifyChanged();
-		}
-	}*/
 
 	@Override
 	protected View onCreateDialogView() {
@@ -213,13 +148,4 @@ public class VerticalAlignSeekbarPref extends DialogPreference {
 		builder.setTitle(null);
 	}
 	
-	/*@Override
-	protected void onDialogClosed(boolean positiveResult) {
-		final float newValue = (float) mSeekBarValue / SEEKBAR_RESOLUTION;
-		if (positiveResult && callChangeListener(newValue)) {
-			setValue(newValue);
-		}
-		super.onDialogClosed(positiveResult);
-	}*/
-
 }
