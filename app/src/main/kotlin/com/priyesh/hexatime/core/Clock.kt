e8954ab2ca8e0ceee6ba3722cde611d83a10c8e8
ItsPriyesh <priyesh.96@hotmail.com>
2015-04-24 16:29:08 -0400

    Add hexadecimal mode and persist settings through reboots

--- app/src/main/kotlin/com/priyesh/hexatime/core/Clock.kt
+++ app/src/main/kotlin/com/priyesh/hexatime/core/Clock.kt
@@ -22,7 +22,9 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Typeface
+import android.preference.PreferenceManager
 import com.priyesh.hexatime.*
+import timber.log.Timber
 import java.util.Calendar
 import kotlin.properties.Delegates
 
@@ -33,6 +35,7 @@ public class Clock(context: Context) : PreferenceDelegate {
     private var enable24Hour = false
     private var enableNumberSign = true
     private var dividerStyle = 0
+    private var enableHexFormat = false;
 
     private final val HOUR = Calendar.HOUR
     private final val HOUR_24 = Calendar.HOUR_OF_DAY
@@ -63,8 +66,17 @@ public class Clock(context: Context) : PreferenceDelegate {
         paint.setAntiAlias(true)
         paint.setTextAlign(Paint.Align.CENTER)
         paint.setColor(Color.WHITE)
-        paint.setTextSize(context.getPixels(50).toFloat())
         paint.setTypeface(Typeface.createFromAsset(context.getAssets(), "Lato-Hairline.ttf"))
+
+        initializeFromPrefs(PreferenceManager.getDefaultSharedPreferences(context))
+    }
+
+    private fun initializeFromPrefs(prefs: SharedPreferences) {
+        enable24Hour = prefs.getBoolean(KEY_ENABLE_24_HOUR, false)
+        enableNumberSign = prefs.getBoolean(KEY_ENABLE_NUMBER_SIGN, true)
+        dividerStyle = prefs.getString(KEY_CLOCK_DIVIDER, "0").toInt()
+        enableHexFormat = prefs.getBoolean(KEY_ENABLE_HEX_FORMAT, false)
+        updateClockSize(prefs.getString(KEY_CLOCK_SIZE, "2").toInt())
     }
 
     override fun onPreferenceChange(prefs: SharedPreferences, key: String) {
@@ -72,6 +84,7 @@ public class Clock(context: Context) : PreferenceDelegate {
             KEY_ENABLE_24_HOUR -> enable24Hour = prefs.getBoolean(key, false)
             KEY_ENABLE_NUMBER_SIGN -> enableNumberSign = prefs.getBoolean(key, true)
             KEY_CLOCK_DIVIDER -> dividerStyle = prefs.getString(key, "0").toInt()
+            KEY_ENABLE_HEX_FORMAT -> enableHexFormat = prefs.getBoolean(key, false)
             KEY_CLOCK_SIZE -> updateClockSize(prefs.getString(key, "2").toInt())
         }
     }
@@ -93,14 +106,20 @@ public class Clock(context: Context) : PreferenceDelegate {
     public fun getTime(): String {
         updateCalendar()
         return "${numberSign()}" +
-                "${formatTwoDigit(hour())}" +
+
+                "${if (enableHexFormat) convertToHex(hour()) else formatTwoDigit(hour())}" +
+
                 "${divider()}" +
-                "${formatTwoDigit(minute())}" +
+
+                "${if (enableHexFormat) convertToHex(minute()) else formatTwoDigit(minute())}" +
+
                 "${divider()}" +
-                "${formatTwoDigit(second())}"
+
+                "${if (enableHexFormat) convertToHex(second()) else formatTwoDigit(second())}"
     }
 
-    private fun formatTwoDigit(num: Int) = java.lang.String.format("%02d", num)
+    private fun convertToHex(num: Int): String = Integer.toHexString(formatTwoDigit(num).toInt())
+    private fun formatTwoDigit(num: Int): String = java.lang.String.format("%02d", num)
 
     private fun updateCalendar() {
         calendar = Calendar.getInstance()
